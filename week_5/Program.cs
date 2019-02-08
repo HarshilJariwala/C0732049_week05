1ead140d5f7149bf9b680dff30714b7a97fa584c
visualstudio <visualstudio@122-PC14>
2019-02-08 18:07:19 -0500

    6 PM Commit

--- week_5/Program.cs
+++ week_5/Program.cs
@@ -10,6 +10,37 @@ namespace week_5
     {
         static void Main(string[] args)
         {
+
+
+
         }
+
+    }
+
+    class node
+    {
+        public node() { }
+        public node floor;
+        public node elevatorup;
+        public  string floornumber;
+    }
+    class elevator
+    {
+        node firstfloor;
+        node secondfloor;
+        node thirdfloor;
+        node fourthfloor;
+
+        public void run()
+        {
+            firstfloor = new node();
+            firstfloor.floornumber = "First Floor";
+            Console.WriteLine
+            firstfloor.elevatorup = secondfloor;
+            secondfloor.floornumber = "Second Floor";
+            
+
+        }
+
     }
 }
