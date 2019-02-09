f9711a36d164ebf7fa6fce9e77b91e661c5fe2e6
visualstudio <visualstudio@122-PC14>
2019-02-08 19:07:25 -0500

    7 PM Commit

--- week_5/Program.cs
+++ week_5/Program.cs
@@ -10,7 +10,9 @@ namespace week_5
     {
         static void Main(string[] args)
         {
-
+            elevator e = new elevator();
+            e.run();
+            Console.ReadKey();
 
 
         }
@@ -34,12 +36,22 @@ namespace week_5
         public void run()
         {
             firstfloor = new node();
+            secondfloor = new node();
+            thirdfloor = new node();
+            fourthfloor = new node();
+
             firstfloor.floornumber = "First Floor";
-            Console.WriteLine
+            Console.WriteLine("The floor number is {0}", firstfloor.floornumber);
             firstfloor.elevatorup = secondfloor;
             secondfloor.floornumber = "Second Floor";
-            
-
+            Console.WriteLine("The floor number is {0}", secondfloor.floornumber);
+            secondfloor.elevatorup = thirdfloor;
+            thirdfloor.floornumber = "Third Floor";
+            Console.WriteLine("The floor number is {0}", thirdfloor.floornumber);
+            thirdfloor.elevatorup = fourthfloor;
+            fourthfloor.floornumber = "Fourth Floor";
+            Console.WriteLine("The floor number is {0}", fourthfloor.floornumber);
+            fourthfloor.elevatorup = null;
         }
 
     }
